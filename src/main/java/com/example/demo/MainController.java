9ca3a5f3fb1347b94c9046a932f6fddff268ab55
groom2hub <groomwnsgur35@gmail.com>
2025-03-10 09:38:55 +0900

    Main page

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import data.dto.BoardDto;
+import data.dto.MovieDto;
+import data.service.BoardService;
+import data.service.MovieService;
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 public class MainController {
+	private final MovieService movieService;
+	private final BoardService boardService;
+	
 	@GetMapping("/")
-	public String goMain()
-	{
-		return "main/mainpage";
-	}
+    public String getMovieInfo(Model model) {
+		List<MovieDto> movieList = movieService.getAllMovies();
+		model.addAttribute("movieList", movieList);
+
+	    List<BoardDto> boardList = boardService.getAllBoards();
+	    model.addAttribute("boardList", boardList);
+        return "main/mainpage";
+    }
 }
